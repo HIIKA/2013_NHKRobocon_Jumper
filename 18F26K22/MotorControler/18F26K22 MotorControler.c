4de12e6516cda88f6f5d6c2915f373892cf6963d
HIIKA <3310670+HIIKA@users.noreply.github.com>
2018-12-18 13:49:51 +0900

    commit for 26K22 ColorLEDControler v2.3.1 DistanceSensorControler v3.6 EncoderControler v2.2.1 JumpControler v1.0 MotorControler v1.0 -> v1.1

--- 18F26K22/MotorControler/18F26K22 MotorControler.c
+++ 18F26K22/MotorControler/18F26K22 MotorControler.c
@@ -2,26 +2,25 @@
 モーター制御部
 モーター制御出力① 	:RC0～RC3
 モーター制御出力② 	:RC4～RC7
-移動中出力ピン		:RB7
-巻取中入力ピン		:RB6
+移動中出力ピン		:RB7 ->ジャンプ制御部へ
+巻取中入力ピン		:RB6 <-ジャンプ制御部へ
 CCP1			:RA4(CCP5)->LED
 CCP2			:RB5(CCP3)->LED
 エラーLEDピン		:RB4
-モーター前進入力 	:RB3
-モーター後進入力 	:RB2
-モーター面舵入力 	:RB1
-モーター裏舵入力 	:RB0
+モーター前進入力 	:RB3 <-センサー制御部より
+モーター後進入力 	:RB2 <-センサー制御部より
+モーター面舵入力 	:RB1 <-センサー制御部より
+モーター取舵入力 	:RB0 <-センサー制御部より
 
 
 ************************************************/
 #include <18f26K22.h>
+#include <limits.h>
 #fuses PLLEN, INTRC_IO,NOWDT,PUT,NOPROTECT,BROWNOUT,NOLVP,MCLR
 
 #use delay(clock = 64000000)
 
-#use fast_io(a)
-#use fast_io(b)
-#use fast_io(c)
+#use fast_io(all)
 
 #define MOVINGPIN Pin_b7
 #define WINDINGPIN Pin_b6
@@ -39,9 +38,37 @@ CCP2			:RB5(CCP3)->LED
 
 int recent = STOP;//現在のモーター状況をバックアップ(input_c()のままだとPWMで認識できなくなる)
 
+
 #inline //この程度の計算はインライン展開させる
 long DetermineDuty(int percent){
 	return (4*(PR2+1)/100)*(long)percent;
+}
+
+void initializing(){
+	//ｵｼﾚｰﾀ設定
+	setup_oscillator(OSC_NORMAL | OSC_64MHZ);
+	//TRIS設定
+	set_tris_a(0);
+	set_tris_b(0x4f);
+	set_tris_c(0);
+	output_a(0x00);
+	output_b(0x00);
+	output_c(0x00);
+	//周期＝（PR2＋１）×4*1/f×（TMR2のプリスケール値）
+	//デューティ＝DC1×1/f×（TMR2のプリスケール値）
+	//duty cycle = value / [ 4 * (PR2 +1 ) ]
+	Setup_ccp5(CCP_PWM | CCP_USE_TIMER1_AND_TIMER2);
+	Setup_ccp3(CCP_PWM | CCP_USE_TIMER1_AND_TIMER2);
+	Setup_timer_0(T0_INTERNAL | T0_DIV_8);
+	Setup_timer_2(T2_DIV_BY_16,PR2,1);//20khz
+	set_timer0(TIMER0INTERVAL);
+	//PWM
+	Set_pwm5_duty(DetermineDuty(100));
+	Set_pwm3_duty(DetermineDuty(100));
+	
+	//割り込み許可
+	enable_interrupts(GLOBAL);
+	enable_interrupts(INT_TIMER0);
 }
 
 void Exception_signal_competition(void){//通信競合時の処理
@@ -60,6 +87,7 @@ void Exception_signal_none(void){
 void mainloop(void){
 	disable_interrupts(INT_TIMER0);//このループ内の処理が伸びた時を考えてループ処理中はﾀｲﾏｰ停止
 	long interval = TIMER0INTERVAL;//通常は20khzで割り込み
+ 	static long DeparturePwmCounter = 10000L;
 	
 	if( (int)input(pin_b0)+(int)input(pin_b1)+(int)input(pin_b2)+(int)input(pin_b3)>1 ){//信号が競合してる
 		Exception_signal_competition();//競合エラー
@@ -77,39 +105,57 @@ void mainloop(void){
 	
 	if(input(pin_b3)){//forward
 		if(recent!=FORWARD){
-			output_c(STOP);
+			output_c(0);//dead time
+			DeparturePwmCounter=0;
 			interval=CHANGEINTERVAL;//1ms変化待ち
+			recent=FORWARD;
 		}else{
 			output_c(FORWARD);
 		}
-		recent=FORWARD;
 	}
 	if(input(pin_b2)){//back
 		if(recent!=BACK){
-			output_c(STOP);
+			output_c(0);//dead time
+			DeparturePwmCounter=0;
 			interval=CHANGEINTERVAL;//1ms変化待ち
+			recent=BACK;
 		}else{
 			output_c(BACK);
 		}
-		recent=BACK;
 	}
 	if(input(pin_b1)){//right
 		if(recent!=RIGHT){
-			output_c(STOP);
+			output_c(0);//dead time
+			DeparturePwmCounter=0;
 			interval=CHANGEINTERVAL;//1ms変化待ち
+			recent=RIGHT;
 		}else{
 			output_c(RIGHT);
 		}
-		recent=RIGHT;
 	}
 	if(input(pin_b0)){//left
 		if(recent!=LEFT){
-			output_c(STOP);
+			output_c(0);//dead time
+			DeparturePwmCounter=0;
 			interval=CHANGEINTERVAL;//1ms変化待ち
+			recent=LEFT;
 		}else{
 			output_c(LEFT);
 		}
-		recent=LEFT;
+	}
+
+	//オーバーフロー対策
+	if(DeparturePwmCounter==LONG_MAX){
+		DeparturePwmCounter=1L;
+	}
+
+	//発信PWM制御
+	if(DeparturePwmCounter++ == 0){
+		Set_pwm5_duty(DetermineDuty(50));
+		Set_pwm3_duty(DetermineDuty(50));
+	}else if(DeparturePwmCounter >= 10000L){
+		Set_pwm5_duty(DetermineDuty(100));
+		Set_pwm3_duty(DetermineDuty(100));
 	}
 
 	//PWM
@@ -166,7 +212,7 @@ void mainloop(void){
 	
 	if(input(pin_b0) ==0 && input(pin_b1) ==0 && input(pin_b2) ==0 && input(pin_b3) ==0){//信号がどれも来てない
 		Exception_signal_none();
-		output_c(STOP);//停止
+		interval=CHANGEINTERVAL;//1ms変化待ち
 		recent=STOP;
 		output_low(MOVINGPIN);
 	}else{
@@ -180,30 +226,7 @@ void mainloop(void){
 
 void main(void)
 {
-	//ｵｼﾚｰﾀ設定
-	setup_oscillator(OSC_NORMAL | OSC_64MHZ);
-	//TRIS設定
-	set_tris_a(0);
-	set_tris_b(0x4f);
-	set_tris_c(0);
-	output_a(0x00);
-	output_b(0x00);
-	output_c(0x00);
-	//周期＝（PR2＋１）×4*1/f×（TMR2のプリスケール値）
-	//デューティ＝DC1×1/f×（TMR2のプリスケール値）
-	//duty cycle = value / [ 4 * (PR2 +1 ) ]
-	Setup_ccp5(CCP_PWM | CCP_USE_TIMER1_AND_TIMER2);
-	Setup_ccp3(CCP_PWM | CCP_USE_TIMER1_AND_TIMER2);
-	Setup_timer_0(T0_INTERNAL | T0_DIV_8);
-	Setup_timer_2(T2_DIV_BY_16,PR2,1);//20khz
-	set_timer0(TIMER0INTERVAL);
-	//PWM
-	Set_pwm5_duty(DetermineDuty(100));
-	Set_pwm3_duty(DetermineDuty(100));
-	
-	//割り込み許可
-	enable_interrupts(GLOBAL);
-	enable_interrupts(INT_TIMER0);
+	initializing();
 	
 	while(true)
 	{
